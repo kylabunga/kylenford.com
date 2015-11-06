6024aec717902bfd5808cc9a784b81e2c7b98e9c
kylabunga <kfordasc@gmail.com>
2015-11-06 14:08:59 -0500

    Just a lightbox
    Scaled back my image gallery plans..

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,57 +1,41 @@
-// external js: isotope.pkgd.js
+// Create a lightbox
 
-$( document ).ready( function() {
-  // init Isotope
-  var $container = $('.isotope').isotope({
-    itemSelector: '.element-item',
-    layoutMode: 'fitRows',
-    getSortData: {
-      name: '.name',
-      symbol: '.symbol',
-      number: '.number parseInt',
-      category: '[data-category]',
-      weight: function( itemElem ) {
-        var weight = $( itemElem ).find('.weight').text();
-        return parseFloat( weight.replace( /[\(\)]/g, '') );
-      }
-    }
-  });
+var $lightbox = $("<div class='lightbox'></div>");
+var $img = $("<img>");
+var $caption = $("<p class='caption'></p>");
 
-  // filter functions
-  var filterFns = {
-    // show if number is greater than 50
-    numberGreaterThan50: function() {
-      var number = $(this).find('.number').text();
-      return parseInt( number, 10 ) > 50;
-    },
-    // show if name ends with -ium
-    ium: function() {
-      var name = $(this).find('.name').text();
-      return name.match( /ium$/ );
-    }
-  };
 
-  // bind filter button click
-  $('#filters').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
-    $container.isotope({ filter: filterValue });
-  });
+// Add image and caption to lightbox
 
-  // bind sort button click
-  $('#sorts').on( 'click', 'button', function() {
-    var sortByValue = $(this).attr('data-sort-by');
-    $container.isotope({ sortBy: sortByValue });
+$lightbox
+  .append($img)
+  .append($caption);
+
+// Add lighbox to document
+
+$('body').append($lightbox);
+
+
+$('.gallery li').click(function (e) {
+  e.preventDefault();
+
+  // Get image link and description
+  var src = $(this).children('img').attr("src");
+  var cap = $(this).children('img').attr("alt");
+
+  // Add data to lighbox
+
+  $img.attr('src',src);
+  $caption.text(cap);
+
+  // $lightbox.append('<img src="' + src + '"></img><p class="caption">' + caption + '</p>');
+
+  // Show lightbox
+
+  $lightbox.show();
+
+  $lightbox.click(function () {
+    $lightbox.hide();
   });
-  
-  // change is-checked class on buttons
-  $('.button-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
-      $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $( this ).addClass('is-checked');
-    });
-  });
-  
 });
+
